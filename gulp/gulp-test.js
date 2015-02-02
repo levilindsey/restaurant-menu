f557112f211d59f5551c234c6909aa32b4fa9432
Levi Lindsey <levisl176@gmail.com>
2015-02-01 23:12:58 -0800

    Removes old Karma testing setup.

--- gulp/gulp-test.js
+++ gulp/gulp-test.js
@@ -3,7 +3,5 @@ var plugins = require('gulp-load-plugins')({lazy: false});
 var config = require('./config');
 
 gulp.task('test', config.buildTasks, function () {
-  return gulp.src(config.allFilesForFrontEndTests)
-      .pipe(plugins.plumber())
-      .pipe(plugins.karma({configFile: config.karmaConfigPath, action: 'run'}));
+  // TODO:
 });
